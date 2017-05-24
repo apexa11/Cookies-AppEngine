7c2c8d572c31a7f4eab149ce2fc35571fc2f58f8
apexa11 <apexachoxi7@gmail.com>
2017-05-24 16:39:36 -0400

    hash-password

--- hash-password/hash-password.py
+++ hash-password/hash-password.py
@@ -10,10 +10,17 @@ def make_salt():
 # HASH(name + pw + salt),salt
 # use sha256
 
-def make_pw_hash(name, pw):
+def make_pw_hash(name, pw, salt = None):
     ###Your code here
+    if not salt:
     salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' %(h,salt)
 
-print make_pw_hash("apexa","patel")
+def valid_pw(name, pw, h):
+    ###Your code here
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name,pw,salt)
+
+h = make_pw_hash('apexa', 'patel')
+print valid_pw('apexa', 'patel', h)
