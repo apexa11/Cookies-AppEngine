1777441e360dcf310354be5080a6a80fe0d0df2c
apexa11 <apexachoxi7@gmail.com>
2017-05-24 11:53:42 -0400

    hash cookies

--- hash-cookie/hash.py
+++ hash-cookie/hash.py
@@ -7,3 +7,12 @@ def hash_val(s):
     return '%s,%s' %(s,hash_str(s))
 
 print hash_val("apexa")
+
+def check_secure_val(h):
+    ###Your code here
+    val = h.split(',')[0]
+    if h == make_secure_val(val):
+        return val
+
+print check_secure_val("apexa,4283e6e0e2cd4456abdd5012e67346f5")
+
